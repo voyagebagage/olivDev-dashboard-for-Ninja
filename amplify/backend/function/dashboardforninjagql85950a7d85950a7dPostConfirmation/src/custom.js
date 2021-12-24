a7f586e1fc52de35407ff17fbd583fca97603da2
Olivier Frugier <voyagebagageao@gmail.com>
2021-12-24 14:09:47 +0700

    add user to cognito group onPost Conf

--- amplify/backend/function/dashboardforninjagql85950a7d85950a7dPostConfirmation/src/custom.js
+++ amplify/backend/function/dashboardforninjagql85950a7d85950a7dPostConfirmation/src/custom.js
@@ -1,6 +1,6 @@
 var aws = require("aws-sdk");
 var ddb = new aws.DynamoDB();
-var cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider();
+var cognitoISP = new aws.CognitoIdentityServiceProvider();
 
 function setParams(event, model, userType, TABLE_NAME) {
   let date = new Date();
@@ -19,13 +19,13 @@ function setParams(event, model, userType, TABLE_NAME) {
 }
 exports.handler = async (event, context, callback) => {
   console.log("event", event);
+
   if (event.request.userAttributes.sub) {
     let userType = event.request.userAttributes["custom:user_type"];
     var CognitoGroupParams = {
       //The name of the group in you cognito user pool that you want to add the user to
       GroupName: userType.charAt(0).toUpperCase() + userType.slice(1),
-      UserPoolId:
-        "arn:aws:cognito-idp:ap-southeast-1:847054957517:userpool/ap-southeast-1_IGu8uTXLT",
+      UserPoolId: event.userPoolId,
       Username: event.userName,
     };
     let params = {};
@@ -48,15 +48,7 @@ exports.handler = async (event, context, callback) => {
         callback("User was not properly confirmed and/or email not verified");
 
       console.log("CognitoGroupParams:", CognitoGroupParams);
-      cognitoidentityserviceprovider.adminAddUserToGroup(
-        CognitoGroupParams,
-        function (err, data) {
-          if (err) {
-            callback(err); // an error occurred
-          }
-          callback(null, event); // successful response
-        }
-      );
+      await cognitoISP.adminAddUserToGroup(CognitoGroupParams).promise();
       console.log("Success");
     } catch (err) {
       console.log("Error", err);
@@ -69,70 +61,3 @@ exports.handler = async (event, context, callback) => {
     return null;
   }
 };
-//--------------------------
-//--------------------------
-//--------------------------
-//--------------------------
-// var params = {
-//   GroupName: "ROLE_ADMIN",
-//   // UserPoolId: 'arn:aws:cognito-idp:us-east-1:23453453453:userpool/us-east-1_XXX',
-//   UserPoolId: "us-east-1_XXX",
-//   // Username: 'user@email.com'
-//   Username: "ec12f604-a83c-4c76-856b-3acd9ca70562",
-// };
-// //--------------------------
-// //--------------------------
-// //--------------------------
-// //--------------------------
-// console.log("before");
-// cognitoidentityserviceprovider.adminAddUserToGroup(
-//   params,
-//   function (err, data) {
-//     console.log(params);
-//     if (err) console.log("Error");
-//     else console.log("Success");
-
-//     // when the action finished
-//     console.log("after");
-
-//     console.log("Executed.");
-
-//     context.succeed(event);
-//   }
-// );
-// //--------------------------
-// //--------------------------
-// //--------------------------
-// //--------------------------
-// // console.log("howdy!",event);
-// var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
-// var params = {
-//   GroupName: "users", //The name of the group in you cognito user pool that you want to add the user to
-//   UserPoolId: event.userPoolId,
-//   Username: event.userName,
-// };
-// //some minimal checks to make sure the user was properly confirmed
-// if (
-//   !(
-//     event.request.userAttributes["cognito:user_status"] === "CONFIRMED" &&
-//     event.request.userAttributes.email_verified === "true"
-//   )
-// )
-//   callback("User was not properly confirmed and/or email not verified");
-// cognitoidentityserviceprovider.adminAddUserToGroup(
-//   params,
-//   function (err, data) {
-//     if (err) {
-//       callback(err); // an error occurred
-//     }
-//     callback(null, event); // successful response
-//   }
-// );
-// if(! (event.request.userAttributes["cognito:user_status"]==="CONFIRMED" && event.request.userAttributes.email_verified==="true") )
-//     callback("User was not properly confirmed and/or email not verified")
-//   cognitoidentityserviceprovider.adminAddUserToGroup(params, function(err, data) {
-//     if (err) {
-//       callback(err) // an error occurred
-//     }
-//     callback(null, event);           // successful response
-//   });
